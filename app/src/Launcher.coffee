1j01/project-nexus

commit f5106608269eb749ac5460d6c1988e79249d4fb3

Cleanup, etc.

app/app.html
@@ -6,8 +6,8 @@
 		<link rel="icon" href="images/icon.png">
 		<link rel="stylesheet" href="lib/octicons/octicons.css">
 		<link rel="stylesheet" href="lib/fontello/css/nex.css">
-		<link rel="stylesheet" href="styles/app.css" class="application">
-		<link rel="stylesheet" href="styles/dark.css" class="application dark">
+		<link rel="stylesheet" href="styles/app.css">
+		<link rel="stylesheet" href="styles/dark.css" class="dark">
 		<link rel="stylesheet" href="../node_modules/elementary.css/lib/elementary.css" class="elementary">
 		<link rel="stylesheet" href="../node_modules/elementary.css/lib/elementary-dark.css" class="elementary dark">
 		<link rel="stylesheet" href="../node_modules/elementary.css/icons/icons.css">

app/src/Launcher.coffee
@@ -45,9 +45,8 @@ class @Launcher extends React.Component
 			scroller = element
 			scroller = scroller.parentElement until scroller.classList.contains "projects"
 			do update_menu_position = =>
-				console.log element.clientHeight
-				# menu.style.top = "#{element.offsetTop + scroller.offsetTop - scroller.scrollTop}px"
 				menu.style.top = "#{element.getBoundingClientRect().bottom}px"
+			setTimeout update_menu_position, 20 # @HACK because it wouldn't position correctly initially
 			window.addEventListener "mousedown", close_menu
 			window.addEventListener "mouseup", close_menu
 			window.addEventListener "keydown", (e)=> @setState menu_open: no if e.keyCode is 27 # Escape

app/src/Terminal.coffee
@@ -3,6 +3,8 @@ Term = Terminal
 
 class @Terminal extends React.Component
 	
+	# @TODO: persistent terminal sessions
+	
 	render: ->
 		E ".terminal-container"
 	
@@ -16,12 +18,11 @@ class @Terminal extends React.Component
 			@init()
 	
 	init: ->
-		# console.log "Terminal::init(), destroy @term"
 		@term?.destroy()
 		
 		proc = @props.process
 		
-		term = new Term
+		@term = new Term
 			cols: 8
 			rows: 2
 			screenKeys: on
@@ -29,7 +30,7 @@ class @Terminal extends React.Component
 		
 		
 		container = React.findDOMNode(@)
-		term.open container
+		@term.open container
 		do resize = => @resize()
 		setTimeout resize, 50
 		window.addEventListener "resize", resize
@@ -39,25 +40,20 @@ class @Terminal extends React.Component
 		# @TODO: use https://github.com/chjj/pty.js
 		
 		proc.stdout.on 'data', (data)=>
-			term.write data
+			@term.write data
 		
 		proc.stderr.on 'data', (data)=>
-			term.write data
+			@term.write data
 		
-		term.on 'data', (data)=>
+		@term.on 'data', (data)=>
 			proc.stdin.write data
 		
 		proc.on 'close', =>
-			term.off 'data'
-		
-		
-		# @TODO: persist terminals
-		# proc.term = term
-		@term = term
+			@term.off 'data'
 	
 	resize: ->
-		if not @term
-			console.log "resize, no @term"
+		unless @term
+			console.log "Terminal::resize, no @term"
 			return
 		
 		container = React.findDOMNode(@)
@@ -79,20 +75,17 @@ class @Terminal extends React.Component
 		tester.innerText = "#"
 		tester_div.appendChild tester
 		
-		console.log "resize",
-			container.clientWidth // tester.clientWidth
-			container.clientHeight // tester.clientHeight
+		w = container.clientWidth // tester.clientWidth
+		h = container.clientHeight // tester.clientHeight
+		console.log "Terminal::resize, #{w}x#{h}"
 		
-		@term.resize(
-			container.clientWidth // tester.clientWidth
-			container.clientHeight // tester.clientHeight
-		)
+		@term.resize w, h
 		
 		container.removeChild tester_terminal
 		tester_terminal = null
 		tester = null
 	
 	componentWillUnmount: ->
-		console.log "destroying term"
+		console.log "Terminal: destroying @term"
 		@term?.destroy()
 		@term = null

app/src/app.coffee
@@ -23,22 +23,16 @@ if win.isTransparent is not Settings.get "elementary"
 
 links = document.querySelectorAll 'link[rel="stylesheet"]'
 update_stylesheets = ->
-	for link in links when link.className.match /application|elementary/
+	for link in links
 		if (link.classList.contains "dark") and not (Settings.get "dark")
-			console.log "remove dark link", link, "because dark theme is disabled"
 			link.remove()
 		else if (link.classList.contains "elementary") and not (Settings.get "elementary")
-			console.log "remove elementary link", link, "because elementary theme is disabled"
 			link.remove()
 		else
 			document.head.appendChild link
 
 Settings.watch "elementary", update_stylesheets
 Settings.watch "dark", update_stylesheets
-	# for link in document.querySelectorAll "link.dark"
-	# 	link.remove()
-	# 
-	# document.getElementById("elementary.css").remove()
 
 {join, resolve} = require "path"
 fs = require "fs"
@@ -53,7 +47,7 @@ Settings.open = no
 do @render = ->
 	React.render (React.createElement ProjectNexus), document.body
 
-# @TODO: watch this directory
+# @TODO: watch the projects directory for changes
 # but don't overwrite the state in the mutated project objects!
 do @read_projects_dir = ->
 	ProjectNexus.projects_read_error = null
@@ -94,7 +88,6 @@ do @read_projects_dir = ->
 							proc.running = no
 						proc.on "exit", (code, signal)->
 							console.log "process #{info} exited with code #{code}"
-							# delete project.processes[command]
 							proc.running = no
 							proc.exitCode = code
 							proc.exitSignal = signal

app/styles/app.css
@@ -20,7 +20,6 @@ main * {
 	margin: 0;
 }
 
-
 .window-frame * {
 	outline: none;
 }
@@ -29,7 +28,8 @@ main {
 	flex-direction: row;
 }
 
-header, li {
+header,
+li {
 	padding: 5px;
 	flex: 0;
 	flex-basis: auto;
@@ -54,7 +54,6 @@ button {
 	flex: 0;
 	flex-basis: auto;
 }
-/* obsolete? */
 header button,
 h2 button,
 li button {
@@ -248,9 +247,6 @@ button.close-process {
 }
 .overlay {
 	background: rgba(0, 0, 0, 0.3);
-	/*background: rgba(100, 100, 100, 0.4);*/
-	/*background: radial-gradient(rgba(0, 0, 0, 0.3));*/
-	/*background: -webkit-radial-gradient(center, ellipse cover, rgba(33,33,33,0.9) 0%, rgba(19,19,19,0) 70%);*/
 }
 .settings {
 	position: absolute;
@@ -305,4 +301,3 @@ button.close-process {
 .setting input[type=text] {
 	flex: 1;
 }
-

app/styles/dark.css
@@ -1,11 +1,8 @@
 
-
 header {
 	background: linear-gradient(to bottom, #77090f 1%,#701602 28%,#590901 60%,#420012 100%);
 	color: white;
 	border-bottom: 1px solid red;
-	/*background: #213841;
-	border-bottom: 1px solid seagreen;*/
 }
 
 main {
@@ -33,6 +30,9 @@ pre {
 	height: 6px;
 }
 ::-webkit-scrollbar-track {
+	background: rgba(0, 0, 0, 0.3);
+}
+::-webkit-scrollbar-corner {
 	background: rgba(0, 0, 0, 0.3);
 }
 ::-webkit-scrollbar-thumb {

app/styles/elementary-layout.css
@@ -1,3 +1,4 @@
+
 .titlebar {
 	display: flex;
 	flex: 0 0 auto;

commit 91eb91544714d19259ae95b124a4820fe5baab5f

Fix things!
Fix Terminal to some degree, broken in
https://github.com/1j01/project-nexus/commit/f5106608269eb749ac5460d6c1988e79249d4fb3

Update dependencies with some fixes

Fix devtools "Detached from target" by only loading one instance of an
entire operating system's styles (with sourcemaps)
https://github.com/nwjs/nw.js/issues/2738

Fix icon reverting to nw.js logo when switching to elementary styles

app/app.html
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" href="lib/fontello/css/nex.css">
 		<link rel="stylesheet" href="styles/app.css">
 		<link rel="stylesheet" href="styles/dark.css" class="dark">
-		<link rel="stylesheet" href="../node_modules/elementary.css/lib/elementary.css" class="elementary">
+		<link rel="stylesheet" href="../node_modules/elementary.css/lib/elementary.css" class="elementary light">
 		<link rel="stylesheet" href="../node_modules/elementary.css/lib/elementary-dark.css" class="elementary dark">
 		<link rel="stylesheet" href="../node_modules/elementary.css/icons/icons.css">
 		<link rel="stylesheet" href="styles/elementary-layout.css">

app/src/Launcher.coffee
@@ -15,7 +15,6 @@ class @Launcher extends React.Component
 		if icon or menu
 			icon ?= "octicon-primitive-dot"
 			E ".launcher",
-				# class: ["has-menu", "menu-open" if @state.menu_open] if menu # haha ReactScript
 				class:
 					"has-menu": menu?
 					"menu-open": @state.menu_open
@@ -102,3 +101,6 @@ class @Launcher extends React.Component
 			window.addEventListener "keydown", (e)=> @setState menu_open: no if e.keyCode is 27 # Escape
 			scroller.addEventListener "scroll", update_menu_position
 			window.addEventListener "resize", update_menu_position
+	
+	componentWillUnmount: ->
+		# @TODO: clean up event listeners

app/src/Terminal.coffee
@@ -22,7 +22,7 @@ class @Terminal extends React.Component
 		
 		proc = @props.process
 		
-		@term = new Term
+		term = @term = new Term
 			cols: 8
 			rows: 2
 			screenKeys: on
@@ -30,8 +30,8 @@ class @Terminal extends React.Component
 		
 		
 		container = React.findDOMNode(@)
-		@term.open container
-		do resize = => @resize()
+		term.open container
+		resize = => @resize()
 		setTimeout resize, 50
 		window.addEventListener "resize", resize
 		container.addEventListener "transitionend", resize, no
@@ -39,17 +39,17 @@ class @Terminal extends React.Component
 		
 		# @TODO: use https://github.com/chjj/pty.js
 		
-		proc.stdout.on 'data', (data)=>
-			@term.write data
+		proc.stdout.on 'data', (data)->
+			term.write data
 		
-		proc.stderr.on 'data', (data)=>
-			@term.write data
+		proc.stderr.on 'data', (data)->
+			term.write data
 		
-		@term.on 'data', (data)=>
+		term.on 'data', (data)->
 			proc.stdin.write data
 		
-		proc.on 'close', =>
-			@term.off 'data'
+		proc.on 'close', ->
+			term.off 'data'
 	
 	resize: ->
 		unless @term
@@ -88,3 +88,4 @@ class @Terminal extends React.Component
 	componentWillUnmount: ->
 		console.log "Terminal: destroying @term"
 		@term?.destroy()
+		@term = null

app/src/app.coffee
@@ -12,6 +12,7 @@ win = window.win = gui.Window.get()
 			toolbar: no
 			frame: not elementary
 			transparent: elementary
+			icon: "app/img/cube.png"
 		window.close()
 	, 50
 
@@ -20,6 +21,8 @@ links = document.querySelectorAll 'link[rel="stylesheet"]'
 update_stylesheets = ->
 	for link in links
 		if (link.classList.contains "dark") and not (Settings.get "dark")
+			link.remove()
+		else if (link.classList.contains "light") and (Settings.get "dark")
 			link.remove()
 		else if (link.classList.contains "elementary") and not (Settings.get "elementary")
 			link.remove()

app/styles/app.css
@@ -313,7 +313,3 @@ pre {
 	border-top-left-radius: 2px;
 	border-bottom-left-radius: 2px;
 }
-/* fix button using default browser styling instead of darkliciousness */
-.window-frame .button {
-	background-color: transparent;
-}